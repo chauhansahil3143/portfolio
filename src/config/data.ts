9b069795cec414dee3f95ac9feee586de3adcec3
chauhansahilcollege <25cp025@bvmengineering.ac.in>
2026-04-30 11:32:56 +0530

    Final fixes

--- src/config/data.ts
+++ src/config/data.ts
@@ -109,7 +109,7 @@ export const projectsData = [
     tags: ['Web Development', 'Python', 'Frontend'],
     github: 'https://github.com/chauhansahil3143/student-aid-search',
     demo: 'https://bvmfind.web.app/',
-    youtube: 'https://www.youtube.com/shorts/xoOApReCWaY',
+    youtube: 'https://youtu.be/O5inlsb3LM0?si=7_Bpck4BthRIsfQ4',
     accent: '#7c5cff',
   },
   {
