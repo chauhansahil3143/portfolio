9f6f76679e7b984dcc4695eaef270cd14d8f9048
chauhansahilcollege <25cp025@bvmengineering.ac.in>
2026-05-02 22:33:53 +0530

    fix: meta cache-busting tags + vercel.json no-cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Cache-busting: forces browsers and WebViews to always fetch fresh HTML */}
+        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
